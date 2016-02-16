ae42ed07253639c8afd24924687287c106970218
Jonathan Tiao <jonathan.tiao17@gmail.com>
2016-02-16 02:41:21 +0000

    catch A and A sharp

--- getch.py
+++ getch.py
@@ -7,14 +7,14 @@ def _find_getch():
 
 	import sys, tty
 	def _getch():
-		fd = sys.stdin.fileno()
-		old_settings = termios.tcgetattr(fd)
+		fd = sys.stdin.fileno();
+		old_settings = termios.tcgetattr(fd);
 		try:
-			tty.setraw(fd)
-			ch = sys.stdin.read(1)
+			tty.setraw(fd);
+			ch = sys.stdin.read(1);
 		finally:
-			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings);
 		
-		return ch
-	return _getch
-getch  = _find_getch()
+		return ch;
+	return _getch;
+getch  = _find_getch();
--- run_keylog.py
+++ run_keylog.py
@@ -1,10 +1,18 @@
 import getch 
+import pygame
 logger = getch.getch
 run = 1;
+
+pygame.init()
+pygame.mixer.init()
+sounda = pygame.mixer.Sound('Sound/piano4_9.wav')
+
 while(run == 1):
 	next_char = logger();
 	if(next_char == '\x03'):
 		run = 0;
 	elif(next_char == 'a'):
-		print "you pressed a!"
+		channela = sounda.play();
+	elif(next_char == 'A'):
+		channela = pygame.mixer.Sound('Sound/piano4_10.wav').play();
 
